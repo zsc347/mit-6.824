8db173d252c1ce8c337f68c240130a3fa750a841
zsc347 <zsc347@163.com>
2018-06-24 13:02:50 +0800

    pass part3: parallel

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -13,22 +16,48 @@ import "fmt"
 //
 func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
 	var ntasks int
-	var n_other int // number of inputs (for reduce) or outputs (for map)
+	var nOther int // number of inputs (for reduce) or outputs (for map)
 	switch phase {
 	case mapPhase:
 		ntasks = len(mapFiles)
-		n_other = nReduce
+		nOther = nReduce
 	case reducePhase:
 		ntasks = nReduce
-		n_other = len(mapFiles)
+		nOther = len(mapFiles)
 	}
 
-	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
+	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nOther)
 
 	// All ntasks tasks have to be scheduled on workers. Once all tasks
 	// have completed successfully, schedule() should return.
-	//
 	// Your code here (Part III, Part IV).
-	//
+	var done sync.WaitGroup
+	for i := 0; i < ntasks; i++ {
+		wk, ok := <-registerChan
+		if ok == false {
+			fmt.Println("Worker chanel closed")
+			return
+		}
+
+		task := DoTaskArgs{}
+		task.JobName = jobName
+		if phase == mapPhase {
+			task.File = mapFiles[i]
+		}
+		task.Phase = phase
+		task.NumOtherPhase = nOther
+		task.TaskNumber = i
+
+		done.Add(1)
+		go func(i int) {
+			ok = call(wk, "Worker.DoTask", task, new(struct{}))
+			if ok == false {
+				fmt.Printf("Worker %s: execute %s phase index %d job error", wk, phase, i)
+			}
+			done.Done()
+			registerChan <- wk
+		}(i)
+	}
+	done.Wait()
 	fmt.Printf("Schedule: %v done\n", phase)
 }
